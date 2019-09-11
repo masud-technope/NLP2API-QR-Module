3b62adc94ecc59cffe217591b5308249a5b6919a
MasudRahman <masudcseku@gmail.com>
2019-09-11 03:35:11 -0600

    Base commit of NLP2API QR module

---
+++ src/nlp2api/code/search/bda/APIClassEntry.java
@@ -0,0 +1,17 @@
+/****
+ * 
+ * @author MasudRahman
+ * This class deals individual API class and corresponding scores during ranking
+ */
+
+
+package nlp2api.code.search.bda;
+
+public class APIClassEntry {
+	public String APIClass;
+	public double bordaScore;
+	public double initQuerySimScore;
+	public double titleSimScore;
+	public double qtextSimScore;
+	public double atextSimScore;
+}
---
+++ src/nlp2api/code/search/bda/QueryToken.java
@@ -0,0 +1,16 @@
+/*****
+ * 
+ * @author MasudRahman
+ *
+ * This class represents each query token and its scores
+ */
+
+package nlp2api.code.search.bda;
+
+public class QueryToken {
+	public String token;
+	public double tokenRankScore=0;
+	public double posRankScore=0;
+	public double totalScore=0;
+	public double bordaScore=0;
+}
